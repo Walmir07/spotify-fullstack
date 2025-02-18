36fc189ddb853c6264a5c9c1e0ac26f12f8077bb
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-02-17 21:25:16 -0300

    Criação de sessão para apresentar artistas e ícone de musicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from './components/Header'
+import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Main></Main>
     </div>
   )
 }
