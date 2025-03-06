7d74dee388bca187df39682e5574d06a9bc6a336
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-03-06 15:52:27 -0300

    Adição de lógica ao botão de Play/Pause e à barra de progresso da música

--- front-end/src/assets/database/songs.js
+++ front-end/src/assets/database/songs.js
@@ -2,7 +2,7 @@
   {
     image: "https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24",
     name: "Última Saudade - Ao Vivo",
-    duration: "2:30",
+    duration: "02:30",
     artist: "Henrique & Juliano",
     audio:
       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
